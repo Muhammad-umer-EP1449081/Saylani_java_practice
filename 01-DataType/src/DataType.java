cee0eecacafa0bd03ad1d6bdf3acc0c59a195967
umer <umer>
2016-07-24 10:29:50 +0500

    building android repository 24-7-2016

---
+++ 01-DataType/src/DataType.java
@@ -0,0 +1,87 @@
+/**
+ * Created by Zeeshan on 4/24/2016.
+ */
+public class DataType {
+    public static void main(String[] args) {
+
+        //Variable Declaration & Initialization
+//        int a;
+//        a = 0;
+//        System.out.println(a);
+
+        //using Datatype : byte
+//        byte a = 100;
+//        byte b = 30;
+//        byte c = (byte)(a+b);
+//        System.out.println("Addition of a and b is byte by casting :"+c);
+
+        //using Datatype : int
+//        int length = 20;
+//        int breadth = 30;
+//        int area = length * breadth;
+//        System.out.println("Area or Quadrilateral = "+area+" sq meter");
+
+        //using Datatype : Char
+//        char a = 'A';
+//        char b = 'a';
+//        char c = (char)(a + b);
+//        System.out.println(c);
+
+//        char ch;
+//        ch = 'X';
+//        System.out.println("ch contains "+ch);
+//
+//        ch++;
+//        System.out.println("ch after increment contains "+ch);
+//
+//        ch = 90;
+//        System.out.println("ch after assigning 90 contains "+ch);
+//
+//        ch = 45+46;
+//        System.out.println("ch after performing addition contains "+ch);
+
+        //using Datatype : float
+//        float a = 2.6f;
+//        int b = (int)a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+        //using Datatype : bool boolean
+//        boolean check;
+//
+//        check = true;
+//        System.out.println("check is "+check);
+//
+//        check = false;
+//        System.out.println("check is "+check);
+//
+//        if(check)
+//            System.out.println("This is not executed as check is false");
+//
+//        check = true;
+//        System.out.println("This is executed as check is True");
+
+
+        //using string
+//        String text = "Hello";
+//        System.out.println(text.charAt(0));
+
+        //Literals
+//        int a =3_2;
+//        System.out.println(a + 2);
+
+//        int hex = 0xfff;        //hexaDecimal literal number start with 0x or 0X
+//        int oct = 022;          //octal literal number start with 0
+//        System.out.println("integer of given hex code = "+hex);
+//        System.out.println("integer of given octal code = "+oct);
+
+//        System.out.println("Hi there! \n This is \"Zeeshan Ayaz\", and i am a \'Java developer\' \nthe technologies on which i am working are: \n \t1.\tGIT\n\t2.\tHTML5 & CSS3\n\t3.\tBootstrap\n\t4.\tJava");
+//        System.out.println("\222 \u22ff");
+
+//        System.out.println("A\tB\tC");
+//        System.out.println("C\tD\tE");
+
+
+
+    }
+}
